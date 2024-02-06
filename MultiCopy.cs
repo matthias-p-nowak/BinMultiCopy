885ecef3b849b7b918ddbe6f93b1cec869776a64
Matthias (generalsjefutvikler) Nowak <matthiaspeter.nowak@centric.eu>
2024-02-06 15:31:54 +0100

    Free to use

---
+++ MultiCopy.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinMultiCopy {
+    class MultiCopy {
+        private List<string> sources;
+        private List<string> targets;
+
+        public MultiCopy(string[] args) {
+            if (args.Length < 3) {
+                Console.Error.WriteLine($"use {Environment.CommandLine} {{<source(file|dir)>}} -t {{<target(<dir>)}}");
+                Environment.Exit(1);
+            }
+            Console.WriteLine($"multicopy for binary files, version {BuildVersion.Version}");
+            sources = new List<string>();
+            targets = new List<string>();
+            var toTarget = false;
+            foreach (string arg in args) {
+                if (string.Equals(arg, "-t")) {
+                    toTarget = true;
+                }
+                else {
+                    if (toTarget)
+                        targets.Add(arg);
+                    else
+                        sources.Add(arg);
+                }
+            }
+        }
+
+        static void Main(string[] args) {
+            var mc = new MultiCopy(args);
+            mc.Run();
+            //Console.ReadLine();
+        }
+
+        private void Run() {
+            var tmp = sources;
+            sources = new List<string>();
+            foreach (string path in tmp) {
+                if (File.Exists(path)) {
+                    sources.Add(path);
+                    Console.WriteLine($"copying specified file {path}");
+                }
+                else if (Directory.Exists(path)) {
+                    Console.WriteLine($"looking for files in {path}");
+                    foreach (var extension in new string[] { "exe", "dll", "json", "config" }) {
+                        var files = Directory.GetFiles(path, $"*.{extension}");
+                        foreach (string fileName in files) {
+                            sources.Add(fileName);
+                            Console.WriteLine($"copying found {extension}-file '{fileName}'");
+                        }
+                    }
+                }
+                else {
+                    Console.Error.WriteLine($"can't find {path}");
+                    Environment.Exit(1);
+                }
+            }
+            tmp = targets;
+            targets = new List<string>();
+            foreach (string path in tmp) {
+                if (File.Exists(path)) {
+                    Console.Error.WriteLine($"{path} should be a directory");
+                    Environment.Exit(1);
+                }
+                else if (!Directory.Exists(path)) {
+                    Directory.CreateDirectory(path);
+                }
+                if (Directory.Exists(path)) {
+                    targets.Add(path);
+                    Console.WriteLine($"copying to {path}");
+                }
+                else {
+                    Console.Error.WriteLine($"target directory {path} does not exist");
+                    Environment.Exit(1);
+                }
+            }
+            Console.WriteLine("----- copying -----");
+            foreach (string src in sources) {
+                Console.WriteLine($"copying {src}");
+                var basename = Path.GetFileName(src);
+                var foe = Path.GetFileNameWithoutExtension(src);
+                foreach (string dest in targets) {
+                    var destFile = Path.Combine(dest, basename);
+                    for (int attempt = 0; attempt < 5; ++attempt) {
+                        try {
+                            try {
+                                File.Copy(src, destFile, true);
+                                break;
+                            }
+                            catch (Exception) {
+                            }
+                            // only if exception
+                            string bakFile = Path.Combine(dest, $"{foe}.x{attempt:d3}");
+                            string tFile = Path.Combine(dest, $"{foe}.tmp");
+                            File.Copy(src, tFile,true);
+                            File.Replace(tFile, destFile, bakFile);
+                            break;
+                        }
+                        catch (Exception ex) {
+                            Console.WriteLine($"got exception {ex.GetType()} while replacing '{destFile}': {ex.Message}");
+                            //try {
+                            //    Console.WriteLine($"deleting {destFile}");
+                            //    File.Delete(destFile);
+                            //}
+                            //catch (Exception) {
+                            //    string tempDest = string.Empty;
+                            //    for (int trial = 0; trial < 100; ++trial) {
+                            //        try {
+                            //            tempDest = foe + $".{trial:d3}";
+                            //            File.Move(destFile, Path.Combine(dest, tempDest));
+                            //            break;
+                            //        }
+                            //        catch (Exception mfex) {
+                            //            Console.WriteLine($"failed to move {destFile} to {tempDest} {mfex.GetType()}: {mfex.Message}");
+                            //            continue;
+                            //        }
+                            //    }
+                            //}
+                        }
+                    }
+                    Console.WriteLine($" -> {destFile}");
+                    var files = Directory.GetFiles(dest, foe + ".0*");
+                    foreach (var fn in files) {
+                        try {
+                            File.Delete(fn);
+                        }
+                        catch (Exception ex) {
+                            Console.WriteLine($"  can't remove {fn} due to {ex.GetType()}: {ex.Message}");
+                        }
+                    }
+                    files = Directory.GetFiles(dest, foe + ".x0*");
+                    foreach (var fn in files) {
+                        try {
+                            File.Delete(fn);
+                        }
+                        catch (Exception ex) {
+                            Console.WriteLine($"  can't remove {fn} due to {ex.GetType()}: {ex.Message}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # BinMultiCopy
-copy common used program files which might be in use
+Copies common used program files which might be in use.
+
+## Background
+
+Files that are in use like DLL's and programs can't be overwritten, since the running program has a lock on those files.
+
+**Solution:** Locked files are locked for writing and therefore cannot be changed. However, it is possible to change the filename by setting the extension to *000* or similar. The normal rename command is also prevented by the lock. Hence, the filename must be *replaced*. This is a change in the directory, not on the file.
+
+## Usage
+
+It is simple, just use the command line with:
+~~~SHELL
+BinMultiCopy.exe {<source(file|dir)>} -t {<target(dir)>}
+~~~
+The first term is the multi copy executable. 
+
+The next arguments can either denote files or directories to be copied. From directories only files ending in *exe*, *dll*, *json*, or *config* are copied.
+
+The next argument is a simple ```-t``` that separates the source directories/files from the target.
+
+The final arguments are a list of directories to copy the discovered files to.
+
+## inner workings
+The program first tries to copy the file into the target directory. If that fails, it copies the file into the directory with extension ".tmp". Then it does a file replacement, renaming the locked target file to ".000" and simultaneously the ".tmp" file into the target file. At most 5 attempts are made.
